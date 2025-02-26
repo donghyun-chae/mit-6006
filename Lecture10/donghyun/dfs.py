783288122d844633f9b6fe7f4859fb8433be3f9e
Donghyun Chae <asena@Donghyuns-Mac-mini.local>
2025-02-26 15:42:23 +0900

    Lecture10 donghyun

---
+++ Lecture10/donghyun/dfs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+def dfs(s, Adj, parent=None, order=None):
+    if parent is None:
+        parent = [None for _ in Adj]
+        parent[s] = s
+        order = []
+
+    for v in Adj[s]:
+        if parent[v] is None:
+            parent[v] = s
+            dfs(v, Adj, parent, order)
+    order.append(s)
+    return parent, order
+
+def full_dfs(Adj):
+    parent = [None for _ in Adj]
+    order = []
+    for v in range(len(Adj)):
+        if parent[v] is None:
+            parent[v] = v
+            dfs(v, Adj, parent, order)
+    return parent, order
+
+print(full_dfs([[],[2,3],[4],[4],[]]))
